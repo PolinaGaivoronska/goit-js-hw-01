7cc620e8f709d63a4183f9b2b45a9588eb12ff7e
Polina Gaivoronska <gaivoronska.art@gmail.com>
2020-08-18 19:05:42 +0300

    Финальные изменения в коде js

--- js/task_01.js
+++ js/task_01.js
@@ -1,12 +1,11 @@
- const name = '«Генератор защитного поля»'; 
- const price = 1000;
-  
- if (price = 1000) {
-    firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
-    console.log(firstMessage);
- }
- 
- else (price = 2000) {
-    secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; 
-    console.log(secondMessage);
- }
+const name = '«Генератор защитного поля»'; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // 
+price = 2000;
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+console.log(firstMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+console.log(secondMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
