18e355b8317d8a1be162e13e11b7c6034bbfdd29
Polina Gaivoronska <gaivoronska.art@gmail.com>
2020-08-18 18:18:51 +0300

    Добавляет файлы html и js

---
+++ js/task_01.js
@@ -0,0 +1,4 @@
+const name = '«Генератор защитного поля»'; // Write code on this line
+const price = 1000; // Write code on this line
+firstMessage = `Выбран $(name), цена за штуку $(price) кредитов`; 
+console.log(firstMessage);// Write code on this line
